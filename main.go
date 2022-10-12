1ec5725868f6754e5b5e2da2e02aa601257dc542
Killian <davittk@tcd.ie>
2022-10-12 15:46:36 +0100

    main.go

--- main.go
+++ main.go
@@ -32,8 +32,8 @@ func main() {
 	filterSizes := []interface{}{1024,2048,4092,8164,40000}
 	sampleSizes := []interface{}{2000}
 	numSites := 100
-	primaryThresholds := []interface{}{0.1,0.05,0.01,0.005,0.001,0.0005}
-	secondaryThresholds := []interface{}{0.1,0.5,0.05,0.001,0.0005}
+	primaryThresholds := []interface{}{0.1,0.05,0.01,0.001,0.0005}
+	secondaryThresholds := []interface{}{0.1,0.5,0.001,0.0005}
 	ps := []interface{}{0.2}
 	qs := []interface{}{0.8}
 	numsHashes := []interface{}{1,32,128}
