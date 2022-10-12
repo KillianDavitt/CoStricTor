958393ccd103a7c9cd739f85a1627d1db5d44f01
Killian <davittk@tcd.ie>
2022-10-12 15:31:16 +0100

    main.go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	httpProp := 0.2
 	filterSizes := []interface{}{1024,2048,4092,8164}
 	sampleSizes := []interface{}{2000}
-	numSites := []interface{}{100}
+	numSites := 100
 	primaryThresholds := []interface{}{0.1,0.05,0.01,0.005,0.001,0.0005}
 	secondaryThresholds := []interface{}{0.1,0.5,0.05,0.001,0.0005}
 	ps := []interface{}{0.2}
