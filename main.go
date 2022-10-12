33225dbbcddcd149e86c89fc3d792fc646d15765
Killian <davittk@tcd.ie>
2022-10-12 16:01:14 +0100

    main.go

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	}
 
 	// Divide the parameters in chunks for the array job
-	numJobs := 10
+	numJobs := 50
 	sizeChunks := int(len(perms)/numJobs)
 	var jobs [][]interface{};
 	jobs, err = chunkSlice(perms, sizeChunks)
