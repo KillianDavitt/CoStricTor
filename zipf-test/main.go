007f18808809ecfe136e40dd96a5a08289c94e01
Killian <davittk@tcd.ie>
2022-10-13 11:11:17 +0100

    zipf-test/main.go

---
+++ zipf-test/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+func main(){
+
+	source := rand.NewSource(time.Now().UnixNano()) 
+	hsts_zipf := rand.NewZipf(rand.New(source), 1.1, 9999.0, 100)
+	
+	numReports := 1000
+	var reports []int = make([]int, 100) 
+	// Sample n sites to report to crews, these can and will be duplicates
+	for i:=0; i<numReports; i++ {
+		n = hsts_zipf.Uint64()
+		reports[n]+=1
+	}
+
+	fmt.Println(reports)
+}
