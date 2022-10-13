df2dedc2145fe6d4155fde6fbd6c4b0a53fea3d5
Killian <davittk@tcd.ie>
2022-10-14 00:24:37 +0100

    main.go

--- main.go
+++ main.go
@@ -29,12 +29,12 @@ func main() {
 	// Some need to be in an interface for the library to get cartesian product of all parameters
 	hstsProp := 0.2
 	httpProp := 0.2
-	filterSizes := []interface{}{4096}
+	filterSizes := []interface{}{4096,8192,16000,32000}
 	sampleSizes := []interface{}{3000000}
 	numSites := 10000
-	//primaryThresholds := []interface{}{0.01,0.005,0.001,0.0005,0.0001,0.00001}
-	primaryThresholds := []interface{}{0.00001,0.000001,0.0000006}
-	secondaryThresholds := []interface{}{0.5,0.1,0.05,0.01,0.005,0.001,0.0005}
+	primaryThresholds := []interface{}{0.01,0.005,0.001,0.0005,0.0001,0.00001,0.000005,0.000001}
+	//primaryThresholds := []interface{}{0.00001,0.000001,0.0000006}
+	secondaryThresholds := []interface{}{0.8,0.5,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001,0.00001}
 	ps := []interface{}{0.1}
 	qs := []interface{}{0.9}
 	numsHashes := []interface{}{1}
