7431158d9c83bcc8067ac2c8daa551affcebeb54
Killian <davittk@tcd.ie>
2022-10-13 11:12:57 +0100

    zipf-test/main.go

--- zipf-test/main.go
+++ zipf-test/main.go
@@ -11,7 +11,7 @@ func main(){
 	source := rand.NewSource(time.Now().UnixNano()) 
 	hsts_zipf := rand.NewZipf(rand.New(source), 1.1, 9999.0, 499)
 	
-	numReports := 1000
+	numReports := 10000
 	var reports []uint = make([]uint, 500) 
 	
 	for i:=0; i<numReports; i++ {
