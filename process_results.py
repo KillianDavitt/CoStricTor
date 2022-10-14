7124a37bdeae8ce898e3ff230549a167050dc083
Killian <davittk@tcd.ie>
2022-10-14 10:35:40 +0100

    process_results.py

---
+++ process_results.py
@@ -0,0 +1,14 @@
+import sys
+with open("crews_output5.csv") as f:
+    data = f.readlines()
+
+d = [x.split(',') for x in data]
+#e = [x for x in d if int(x[2])==0]
+
+a = sorted(e, key=lambda x:int(x[1]/x[2]), reverse=True)
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
