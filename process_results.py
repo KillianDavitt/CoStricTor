010c113177151f1d1483c83fd4dc9a3cc0d2a871
Killian <davittk@tcd.ie>
2022-10-14 10:39:09 +0100

    process_results.py

--- process_results.py
+++ process_results.py
@@ -3,7 +3,7 @@ with open("results/run_7/crews_full_output.csv") as f:
     data = f.readlines()
 
 d = [x.split(',') for x in data]
-e = [x for x in d if int(x[2])==0]
+e = [x for x in d if int(x[2])!=0]
 
 a = sorted(e, key=lambda x:float(x[1])/float(x[2]), reverse=True)
 print(a[0])
