ac1921e1473547ea3626f70b7e125e249f44f13a
Killian <davittk@tcd.ie>
2022-10-21 15:33:46 +0100

    main.go

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	filterSizes := []interface{}{8192}
 	sampleSizes := []interface{}{3000000}
 	numSites := 10000
-	primaryThresholds := []interface{}{0.0001}
+	primaryThresholds := []interface{}{0.00005}
 	secondaryThresholds := []interface{}{0.8,0.5,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001,0.00001}
 	ps := []interface{}{0.0}
 	qs := []interface{}{1.0}
