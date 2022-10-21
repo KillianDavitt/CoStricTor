218c33078d19b47030b36437c18b3f9d537b9189
Killian <davittk@tcd.ie>
2022-10-21 21:35:56 +0100

    main.go

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	// Some need to be in an interface for the library to get cartesian product of all parameters
 	hstsProp := 0.2
 	httpProp := 0.2
-	filterSizes := []interface{}{22000}
+	filterSizes := []interface{}{35000}
 	sampleSizes := []interface{}{3000000}
 	numSites := 10000
 	primaryThresholds := []interface{}{0.00001}
