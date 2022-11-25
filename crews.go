74d38f07c51dcf83b919933979976d51c02365db
Killian <davittk@tcd.ie>
2022-11-25 15:57:39 +0000

    crews.go

--- crews.go
+++ crews.go
@@ -37,6 +37,6 @@ func (c *Crews) PrimaryTest(s string) bool {
 func (c *Crews) SecondaryTest(s string) bool {
 	count := c.secondary.Test([]byte(s))
 	adjustedCount := uint((float64(count) - c.p * float64(c.secondary.count))/(c.q-c.p))
-	return  adjustedCount >= uint((c.secondary.count/c.numWebsites))
+	return  adjustedCount >= uint((c.secondary.count/c.numWebsites)*c.thresholdModifer)
 }
 
