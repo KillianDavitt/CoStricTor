71a603f4be0fc2311a350431f34a551933c9857f
Killian <davittk@tcd.ie>
2022-11-29 18:14:07 +0000

    main.go

--- main.go
+++ main.go
@@ -7,31 +7,10 @@ import (
 	"sync"
 	"fmt"
 	"strconv"
-	"flag"
-	"log"
-	"runtime"
-	"runtime/pprof"
-	//"net/http/pprof"
 )
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 
 func main() {
-
-flag.Parse()
-    if *cpuprofile != "" {
-        f, err := os.Create(*cpuprofile)
-        if err != nil {
-            log.Fatal("could not create CPU profile: ", err)
-        }
-        defer f.Close() // error handling omitted for example
-        if err := pprof.StartCPUProfile(f); err != nil {
-            log.Fatal("could not start CPU profile: ", err)
-        }
-        defer pprof.StopCPUProfile()
-    }
 	
 	// Load in the big list of websites 
 	const path string = "websites.txt"
@@ -98,19 +77,6 @@ flag.Parse()
 		go runSim(params.([]interface{}), hsts, http, https_no_hsts, &wg, hstsProp, httpProp, numSites)
 	}
 	wg.Wait()
-
-
-	if *memprofile != "" {
-        f, err := os.Create(*memprofile)
-        if err != nil {
-            log.Fatal("could not create memory profile: ", err)
-        }
-        defer f.Close() // error handling omitted for example
-        runtime.GC() // get up-to-date statistics
-        if err := pprof.WriteHeapProfile(f); err != nil {
-            log.Fatal("could not write memory profile: ", err)
-        }
-    }
 }
 
 func chunkSlice(slice []interface{}, chunkSize int) ([][]interface{}, error) {
