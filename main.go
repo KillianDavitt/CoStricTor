a6e77560b40aaaf0031578484b0cb663f5f15543
Killian <davittk@tcd.ie>
2022-11-29 16:59:51 +0000

    main.go

--- main.go
+++ main.go
@@ -15,6 +15,19 @@ var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 
+flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal("could not create CPU profile: ", err)
+        }
+        defer f.Close() // error handling omitted for example
+        if err := pprof.StartCPUProfile(f); err != nil {
+            log.Fatal("could not start CPU profile: ", err)
+        }
+        defer pprof.StopCPUProfile()
+    }
+	
 	// Load in the big list of websites 
 	const path string = "websites.txt"
 	file, err := os.Open(path)
@@ -80,6 +93,19 @@ func main() {
 		go runSim(params.([]interface{}), hsts, http, https_no_hsts, &wg, hstsProp, httpProp, numSites)
 	}
 	wg.Wait()
+
+
+	if *memprofile != "" {
+        f, err := os.Create(*memprofile)
+        if err != nil {
+            log.Fatal("could not create memory profile: ", err)
+        }
+        defer f.Close() // error handling omitted for example
+        runtime.GC() // get up-to-date statistics
+        if err := pprof.WriteHeapProfile(f); err != nil {
+            log.Fatal("could not write memory profile: ", err)
+        }
+    }
 }
 
 func chunkSlice(slice []interface{}, chunkSize int) ([][]interface{}, error) {
