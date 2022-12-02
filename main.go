79ac309e52a6ec293b8b24559abf375f4346d1af
Killian <davittk@tcd.ie>
2022-12-02 12:43:58 +0000

    main.go

--- main.go
+++ main.go
@@ -37,6 +37,7 @@ func main() {
 	perms := make([]interface{},len(filterSizes))
 	for i:=0; i<len(filterSizes); i++ {
 		p := []interface{}{filterSizes[i],3000000,0.00001,0.9,0.02,0.1}
+		fmt.Println(i)
 		perms = append(perms,p)
 	}
 	fmt.Println(len(filterSizes))
