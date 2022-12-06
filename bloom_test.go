67c2aeb6693e50a0b9aba714994e0c1918c972c5
Killian <davittk@tcd.ie>
2022-12-06 16:28:40 +0000

    bloom_test.go

--- bloom_test.go
+++ bloom_test.go
@@ -3,8 +3,18 @@ package main
 import "testing"
 
 func TestNewFilter(t *testing.T) {
-	b := NewBloomFilter(5000, 8)
+	h:=20
+	b := NewBloomFilter(5000, h)
 	data := []byte("Hi")
 	b.Add(data, 0, 1)
 	t.Errorf("hi")
+
+	d = b.data
+	sumBits := 0
+	for i:=0; i<len(d); i++ {
+		sumBits += d[i]
+	}
+	if sumBits != h {
+		t.Errorf("Perturbation happening for q=1, p=0")
+	}
 }
