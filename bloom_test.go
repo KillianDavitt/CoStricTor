6b0fe675faf97ca6aeecfb8d705f8d5a38e76642
Killian <davittk@tcd.ie>
2022-12-06 16:31:43 +0000

    bloom_test.go

--- bloom_test.go
+++ bloom_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestNewFilter(t *testing.T) {
-	var hs := [5]uint{1,4,6,18,22}
+hs := [5]uint{1,4,6,18,22}
 	for i:=0; i<len(hs); i++ {
 		b := NewBloomFilter(5000, hs[i])
 		data := []byte("Hi")
