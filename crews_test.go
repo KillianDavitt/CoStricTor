67cfedc973f387fe5dd44f5463cee0fcceab7224
Killian <davittk@tcd.ie>
2022-12-06 16:39:05 +0000

    crews_test.go

--- crews_test.go
+++ crews_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestNewCrews(t *testing.T) {
 	c := NewCrews(1000, 8, 50, 0.01, 0.01, 0, 1)
 	c.ReportHsts("www.google.com")
-	if c.b.data != sumFilter(c.primary) {
+	if 8 != sumFilter(c.primary) {
 		t.Errorf("Error in reporting")
 	}
 }
