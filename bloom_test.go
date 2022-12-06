94b8600f9f66095344c9a322976ce6bb69f4cd10
Killian <davittk@tcd.ie>
2022-12-06 16:30:39 +0000

    bloom_test.go

--- bloom_test.go
+++ bloom_test.go
@@ -3,17 +3,19 @@ package main
 import "testing"
 
 func TestNewFilter(t *testing.T) {
-	var h uint =20
-	b := NewBloomFilter(5000, h)
-	data := []byte("Hi")
-	b.Add(data, 0, 1)
+	var hs []uint = [1,4,6,18,22]
+	for i:=0; i<len(hs); i++ {
+		b := NewBloomFilter(5000, h)
+		data := []byte("Hi")
+		b.Add(data, 0, 1)
 
-	d := b.data
-	var sumBits uint = 0
-	for i:=0; i<len(d); i++ {
-		sumBits += d[i]
-	}
-	if sumBits != h {
-		t.Errorf("Perturbation happening for q=1, p=0")
+		d := b.data
+		var sumBits uint = 0
+		for i:=0; i<len(d); i++ {
+			sumBits += d[i]
+		}
+		if sumBits != h {
+			t.Errorf("Perturbation happening for q=1, p=0")
+		}
 	}
 }
