58a45583529d610a01307e9788e3c6dcd1987d48
Killian <davittk@tcd.ie>
2022-12-06 16:38:20 +0000

    crews_test.go

--- crews_test.go
+++ crews_test.go
@@ -5,7 +5,9 @@ import "testing"
 func TestNewCrews(t *testing.T) {
 	c := NewCrews(1000, 8, 50, 0.01, 0.01, 0, 1)
 	c.ReportHsts("www.google.com")
-	if c.b.data
+	if c.b.data != sumFilter(c.b) {
+		t.Error
+	}
 }
 
 func sumFilter(b *BloomFilter) (int) {
