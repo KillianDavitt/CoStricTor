0927d090aaa90f54f5305985ab225cc9d26752df
Killian <davittk@tcd.ie>
2023-01-05 13:42:46 +0000

    main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 	//secondaryThresholds := [6]float64{0.01,0.02,0.03,0.04,0.05,0.06}
 	//secondaryThreshs := [16]float64{0.1,0.2,0.3,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.02,0.1,0.001,0.005,0.0005,0.0001}
 	numSites := 10000
-	numJobs := 1
+	numJobs := 6
 
 	var perms []interface{};
 	for i:=0; i<len(filterSizes); i++ {
