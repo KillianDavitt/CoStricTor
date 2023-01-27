c9f402f2222ae76cb96c4c2b7421a4dbfb94d912
Killian <davittk@tcd.ie>
2023-01-27 16:48:12 +0000

    p_from_q.py

--- p_from_q.py
+++ p_from_q.py
@@ -3,7 +3,7 @@ import math
 def p(q,ep):
     e_ep = math.e**ep
     
-    return q/e_ep-(e_ep*q)+q
+    return q/(e_ep-(e_ep*q)+q)
   
 def q(p,ep):
     return (p * math.e ** ep)/(1-p+p*math.e**ep)
