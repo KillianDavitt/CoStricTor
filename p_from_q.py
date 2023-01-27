845933a9c785c5afa12b9c8bdd831e655468daa9
Killian <davittk@tcd.ie>
2023-01-27 13:50:15 +0000

    p_from_q.py

---
+++ p_from_q.py
@@ -0,0 +1,19 @@
+import math
+  
+def p(q,ep):
+    return (q)/((1-q)*(math.e**ep + q))
+  
+def q(p,ep):
+    return (p * math.e ** ep)/(1-p+p*math.e**ep)
+  
+#return (p(0.9,7))
+#return q(0.0082002,7)
+  
+qs = [0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5]
+ps = []
+
+eps = [9,8,7,6,5,4,3,2,1]
+for i in eps:
+    ps.append(p(0.9,i))
+  
+print(ps)
