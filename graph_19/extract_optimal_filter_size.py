f080d2746833bb508fbcc27155850210471077be
Killian <davittk@tcd.ie>
2023-01-27 17:35:20 +0000

    graph_19/extract_optimal_filter_size.py

--- graph_19/extract_optimal_filter_size.py
+++ graph_19/extract_optimal_filter_size.py
@@ -6,3 +6,7 @@ data = [x.split(',') for x in d]
 qualifies = lambda x: x[2]<(x[1]/10)
 
 first_filter = [x for x in data if qualifies(x)]
+
+sorted_results = sorted(first_filter, key=lambda x: x[1])
+
+print(sorted_results[0]
