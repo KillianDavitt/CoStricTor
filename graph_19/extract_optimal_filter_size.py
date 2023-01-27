8b4e32b4790f9ff1a9a91136fa0c9cbad7f407e9
Killian <davittk@tcd.ie>
2023-01-27 17:37:43 +0000

    graph_19/extract_optimal_filter_size.py

--- graph_19/extract_optimal_filter_size.py
+++ graph_19/extract_optimal_filter_size.py
@@ -2,7 +2,7 @@ with open('crews_full_output.csv') as f:
     d = f.readlines()
 
 data = [x.split(',') for x in d]
-d1 = [x for x in data if x[8]='0.0003700923931708333']
+d1 = [x for x in data if x[8]=='0.0003700923931708333']
 qualifies = lambda x: int(x[2])<(int(x[1])/10)
 
 first_filter = [x for x in d1 if qualifies(x)]
