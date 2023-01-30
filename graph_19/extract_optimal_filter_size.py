5dc7167542587f1faf165e8a261e0096d5e696d2
Killian <davittk@tcd.ie>
2023-01-30 15:49:28 +0000

    graph_19/extract_optimal_filter_size.py

--- graph_19/extract_optimal_filter_size.py
+++ graph_19/extract_optimal_filter_size.py
@@ -1,4 +1,5 @@
 import math
+            import matplotlib.pyplot as plt
 
 def get_ep(q,p):
     one_minus_p = 1-p
@@ -13,37 +14,40 @@ with open('crews_full_output.csv') as f:
 raw_data = [x.split(',') for x in d]
 
 p_s = list(set([x[8] for x in raw_data]))
-
-results_per_p = []
-
-for i in range(len(p_s)):
-    to_add = [x for x in raw_data if x[8]==p_s[i]]
-    results_per_p.append(to_add)
+filter_sizes = list(set([x[7] for x in raw_data]))
 
 
-qualifies = lambda x: int(x[2])<(int(x[1])/1.5)
+for f in filter_sizes:
+    results_per_p = []
 
-end_results = []
-## for every p in the test, find the optimal filter size
-for d in results_per_p:
+    for i in range(len(p_s)):
+        to_add = [x for x in raw_data if x[8]==p_s[i]]
+        results_per_p.append(to_add)
+
+
+        qualifies = lambda x: int(x[2])<(int(x[1])/1.5)
+
+        end_results = []
+        ## for every p in the test, find the optimal filter size
+        for d in results_per_p:
     
-    first_filter = [x for x in d if qualifies(x)]
-    if len(first_filter)<1:
-        print("none qualified")
-    sorted_results = sorted(first_filter, key=lambda x: x[1])
+            first_filter = [x for x in d if qualifies(x)]
+            if len(first_filter)<1:
+                print("none qualified")
+            sorted_results = sorted(first_filter, key=lambda x: x[1])
 
-    end_results.append(sorted_results[0])
+            end_results.append(sorted_results[0])
 
-print(end_results)
-import matplotlib.pyplot as plt
+            print(end_results)
 
 
-end = sorted(end_results, key=lambda x: get_ep(0.75,float(x[8])))
-for e in end:
 
-    x = e[7] 
-    y = e[1]
-    plt.scatter(x,y, label=e[0])
+            end = sorted(end_results, key=lambda x: get_ep(0.75,float(x[8])))
+            for e in end:
+
+                x = e[7] 
+                y = e[1]
+                plt.scatter(x,y, label=e[0])
 
 
     
