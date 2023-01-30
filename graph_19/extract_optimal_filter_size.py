e5ce819d75611b109ae7bce07c16ee7a99584e34
Killian <davittk@tcd.ie>
2023-01-30 15:59:50 +0000

    graph_19/extract_optimal_filter_size.py

--- graph_19/extract_optimal_filter_size.py
+++ graph_19/extract_optimal_filter_size.py
@@ -24,27 +24,27 @@ for i in range(len(p_s)):
     to_add = [x for x in raw_data if x[8]==p_s[i]]
     results_per_p.append(to_add)
 
+    for f in filter_sizes:
+        qualifies = lambda x: int(x[2])<(int(x[1]) and x[7]==f)
 
-    qualifies = lambda x: int(x[2])<(int(x[1]))
-
-    end_results = []
-    ## for every p in the test, find the optimal filter size
-    for d in results_per_p:
+        end_results = []
+        ## for every p in the test, find the optimal filter size
+        for d in results_per_p:
     
-        first_filter = [x for x in d if qualifies(x)]
-        if len(first_filter)<1:
-            print("none qualified")
-        sorted_results = sorted(first_filter, key=lambda x: x[1])
+            first_filter = [x for x in d if qualifies(x)]
+            if len(first_filter)<1:
+                print("none qualified")
+            sorted_results = sorted(first_filter, key=lambda x: x[1])
 
-        end_results.append(sorted_results[0])
-
-        #print(end_results)
-        end = sorted(end_results, key=lambda x: get_ep(0.75,float(x[8])))
-        for e in end:
+            end_results.append(sorted_results[0])
             
-            x = e[7] 
-            y = e[1]
-            plt.plot(x,y, label=e[0])
+            #print(end_results)
+            end = sorted(end_results, key=lambda x: get_ep(0.75,float(x[8])))
+            for e in end:
+            
+                x = e[7] 
+                y = e[1]
+                plt.plot(x,y, label=e[0])
 
             
 #plt.ylim(0,3000)
