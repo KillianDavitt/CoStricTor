28097c8a89371def1514f362c7b4f3d748d460a2
Killian <davittk@tcd.ie>
2023-01-31 15:49:47 +0000

    main.go

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	var perms []interface{};
 	for i:=0; i<len(filterSizes); i++ {
 		for j:=0; j<len(ps); j++ {
-			p := []interface{}{filterSizes[i],3000000,ps[j],qs[0],0.0005,0.07}
+			p := []interface{}{filterSizes[i],3000000,ps[j],qs[0],0.00005,0.07}
 			perms = append(perms,p)
 		}
 	}
