aab87cfe1ec76c350ecde7010cbb5bb5e840eebf
Killian <davittk@tcd.ie>
2023-02-03 15:51:43 +0000

    graph_20/CREWS.py

--- graph_20/CREWS.py
+++ graph_20/CREWS.py
@@ -36,7 +36,7 @@ fig.suptitle('CREWS Results')
 NUM_COLORS = len(qs)
 
 cm = plt.get_cmap('gist_rainbow')
-print(cm)
+print(cm[0])
 fig = plt.figure()
 axs[0].set_prop_cycle('color', [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
 
