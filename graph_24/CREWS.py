881c551596695a384c22bcb6f850659b62130d18
Killian <davittk@tcd.ie>
2023-02-16 16:37:37 +0000

    graph_24/CREWS.py

--- graph_24/CREWS.py
+++ graph_24/CREWS.py
@@ -68,7 +68,7 @@ axs[0].set(xlabel='Filter Size', ylabel='Upgrades')
 axs[1].set(xlabel='Filter Size', ylabel='Disasters')
 axs[2].set(xlabel='Filter Size', ylabel='Additional FPs')
 
-labels = [str(get_ep(p)) for p in ps]
+labels = [str(round(get_ep(p))) for p in ps]
 axs[0].legend(labels,title='epsilon',bbox_to_anchor=(1.0001, 1.0501))
 #axs[1].legend(labels)
 #axs[2].legend(labels)
